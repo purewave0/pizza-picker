602a75f9725a8d4810f151d8f79b0f62b32457b1
purewave0 <purewave_@outlook.com>
2025-04-01 23:47:52 -0300

    feat: allow visually selecting flavours

--- static/js/index.js
+++ static/js/index.js
@@ -2,42 +2,54 @@ document.addEventListener('DOMContentLoaded', () => {
     const pizzaElement = document.getElementById('pizza');
     const pizza = new PizzaPicker(pizzaElement);
 
-    const pizzaSizeInfo = {
+    const pizzaSizesInfo = {
         'small': {
             slicesAmount: 4,
             maxFlavours: 1,
             description: 'Just the right size for a solo pizza night.',
             serving: 'Serves 1-2 people',
+            maxFlavoursText: 'Single flavour',
+            flavourPickerText: 'Choose a flavour:',
         },
         'medium': {
             slicesAmount: 6,
             maxFlavours: 2,
             description: 'Ideal for sharing with friends.',
             serving: 'Serves 2-3 people',
+            maxFlavoursText: 'Up to 2 flavours',
+            flavourPickerText: 'Choose up to 2 flavours:',
         },
         'large': {
             slicesAmount: 8,
             maxFlavours: 2,
             description: 'Perfect for gatherings and satisfying appetites.',
             serving: 'Serves 3-4 people',
+            maxFlavoursText: 'Up to 2 flavours',
+            flavourPickerText: 'Choose up to 2 flavours:',
         },
         'extra-large': {
             slicesAmount: 12,
             maxFlavours: 3,
             description: 'The ultimate choice for big celebrations.',
             serving: 'Serves 4-6 people',
+            maxFlavoursText: 'Up to 3 flavours',
+            flavourPickerText: 'Choose up to 3 flavours:',
         },
     };
 
-    const sizes = document.getElementById('sizes').children;
+    const sizesParent = document.getElementById('sizes');
     let currentlySelectedSize = null;
+    const flavoursParent = document.getElementById('flavours');
+    let currentlySelectedFlavours = [];
+    let maxPizzaFlavours = null;
 
     pizzaSizeDescription = document.getElementById('pizza-size-description');
     pizzaServing = document.getElementById('pizza-serving');
     pizzaFlavour = document.getElementById('pizza-flavour');
+    flavourPickerText = document.getElementById('flavour-text');
 
     function updatePizzaBySize(size) {
-        const sizeInfo = pizzaSizeInfo[size];
+        const sizeInfo = pizzaSizesInfo[size];
 
         pizzaSizeDescription.textContent = sizeInfo.description;
         pizzaServing.textContent = sizeInfo.serving;
@@ -48,12 +60,22 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             flavourInfo = `Pick up to ${sizeInfo.maxFlavours} flavours.`;
         }
-        pizzaFlavour.textContent = flavourInfo;
+        pizzaFlavour.textContent = sizeInfo.maxFlavoursText;
+        maxPizzaFlavours = sizeInfo.maxFlavours;
+
+        // reset flavours
+        for (const flavour of currentlySelectedFlavours) {
+            flavour.classList.remove('selected');
+        }
+        currentlySelectedFlavours = [];
+        flavoursParent.classList.remove('maxed-out');
+
+        flavourPickerText.textContent = sizeInfo.flavourPickerText;
 
         pizza.setSlicesAmount(sizeInfo.slicesAmount)
     }
 
-    for (const size of sizes) {
+    for (const size of sizesParent.children) {
         if (size.classList.contains('selected')) {
             currentlySelectedSize = size;
         }
@@ -69,6 +91,70 @@ document.addEventListener('DOMContentLoaded', () => {
             size.classList.add('selected');
 
             updatePizzaBySize(size.dataset.size);
+        });
+    }
+
+    const pizzaFlavoursInfo = {
+            // TODO
+        'pepperoni': {
+        },
+        'buffalo-chicken':{
+        },
+        'margherita': {
+        },
+        'cheese': {
+        },
+        'veggie-supreme': {
+        },
+        'neapolitan': {
+        },
+        'sausage': {
+        },
+        'mushrooms': {
+        },
+    };
+
+    function updatePizzaFlavours(flavours) {
+        // TODO
+    }
+
+    for (const flavour of flavoursParent.children) {
+        flavour.addEventListener('click', () => {
+            if (flavour.classList.contains('selected')) {
+                // deselect
+                flavour.classList.remove('selected');
+                currentlySelectedFlavours = currentlySelectedFlavours
+                    .filter(f => f.dataset.flavour !== flavour.dataset.flavour);
+
+                // in case it was maxed out before
+                flavoursParent.classList.remove('maxed-out');
+                return;
+            }
+
+            const isSingleChoice = maxPizzaFlavours === 1;
+            if (isSingleChoice) {
+                const isMaxedOut =
+                    currentlySelectedFlavours.length === maxPizzaFlavours;
+                if (isMaxedOut) {
+                    /* when it's single choice and it's maxed out, simply deselect the
+                       old one */
+                    currentlySelectedFlavours[0].classList.remove('selected');
+                    currentlySelectedFlavours.shift();
+                }
+            } else {
+                const willMaxOut =
+                    currentlySelectedFlavours.length+1 === maxPizzaFlavours;
+                if (willMaxOut) {
+                    /* when it's single choice and it'll max out, disallow any further
+                       selections until at least one of them is deselected */
+                    flavoursParent.classList.add('maxed-out');
+                }
+            }
+
+            currentlySelectedFlavours.push(flavour);
+            flavour.classList.add('selected');
+
+            updatePizzaFlavours(currentlySelectedFlavours.map(f => f.dataset.flavour));
         });
     }
 
