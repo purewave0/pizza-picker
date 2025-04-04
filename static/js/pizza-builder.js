562e72bb5fd0b42956b75f6e077948bb86a525bf
purewave0 <purewave_@outlook.com>
2025-04-04 17:52:33 -0300

    docs(pizza-builder): add documentation

--- static/js/pizza-builder.js
+++ static/js/pizza-builder.js
@@ -1,3 +1,10 @@
+/**
+ * Allows viewing and customizing a pizza. Customizable aspects include:
+ *
+ * - amount of slices
+ * - slices' gap width
+ * - topping image textures
+ */
 class PizzaBuilder {
     #chart;
     #data;
@@ -5,6 +12,9 @@ class PizzaBuilder {
 
     static #COLOR_NO_FLAVOUR_SELECTED = 'rgba(127, 127, 127, 0.7)';
 
+    /**
+     * @param {Node} target The target element. Must be a Canvas.
+     */
     constructor(target) {
         this.#data = {
             datasets: [{
@@ -27,17 +37,32 @@ class PizzaBuilder {
         this.#chart = new Chart(target, config);
     }
 
+    /**
+     * Set the width, in pixels, of the gap between each slice.
+     */
     setSliceGapWidth(gapWidth) {
         this.#chart.options.borderWidth = gapWidth;
         this.#chart.update();
     }
 
+    /**
+     * Set the amount of slices. Each slice will be of equal size.
+     */
     setSlicesAmount(amount) {
         this.#data.datasets[0].data = new Array(amount).fill(1);
         this.#chart.update();
         this.#slicesAmount = amount;
     }
 
+    /**
+     * Return a CanvasPattern of the given size out of the given URL. This is meant to
+     * be used when setting the "backgroundColor" of the underlying chart, as ChartJS
+     * allows setting CanvasPatterns as the chart background.
+     *
+     * @param {string} imageURL The URL to the image.
+     * @param {number} size The size, in pixels, for the CanvasPattern's width and
+     *     height.
+     */
     static #createCanvasPatternFromImageUrl(imageUrl, size) {
         return new Promise((resolve, reject) => {
             const canvas = document.createElement('canvas');
@@ -58,6 +83,16 @@ class PizzaBuilder {
         });
     }
 
+    /**
+     * Set the pizza's topping texture(s). If more than 1 texture is given, the amount
+     * of slices must be a multiple of the amount of textures. This ensures that each
+     * topping is distributed evenly across the number of slices.
+     *
+     * @param {?Array<string>} textureUrls The array of texture URLs. If null, the whole
+     *     pizza's background will be set to a default color.
+     * @param {number} textureSize The size, in pixels, for the texture's width and
+     *     height.
+     */
     setToppingTextures(textureUrls, textureSize) {
         if (textureUrls === null) {
             this.#data.datasets[0].backgroundColor =
@@ -90,6 +125,7 @@ class PizzaBuilder {
             }
         }
 
+        // update once all textures load
         Promise.all(textureUpdatePromises).then(() => {
             this.#chart.update();
         });
